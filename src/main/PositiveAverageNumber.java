ef39d1719e29f3e653ab8f09d3520148e176068f
DannyBoy13th <danielsolo13th@gmail.com>
2016-05-18 17:58:58 +0300

    Update PositiveAverageNumber.java

--- src/main/PositiveAverageNumber.java
+++ src/main/PositiveAverageNumber.java
@@ -4,7 +4,9 @@ package main;
  * Created by Daniel Solo on 18.05.2016.
  */
 public class PositiveAverageNumber {
+   
     public int average(int a, int b) {
+        
         int numbers = a%2 + b%2;
         int result = a/2 + b/2;
         return result + numbers/2;
